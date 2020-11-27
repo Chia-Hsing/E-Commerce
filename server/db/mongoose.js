1816f77aa52c166fba983c4bf72f902662dd644b
Chia-Hsing <ausgeflippte@gmail.com>
2020-11-28 01:02:28 +0800

    start building server side api

---
+++ server/db/mongoose.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+mongoose
+    .connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+    })
+    .then(() => console.log('DB connect successfully!'))
+    .catch(e => {
+        console.log(e)
+    })
