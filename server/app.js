080182f020bd6959794d496e479bb1ca293daa1b
Chia-Hsing <ausgeflippte@gmail.com>
2021-02-21 14:48:22 +0800

    deploy

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ require('./routers/index')(app)
 
 /* There is also a built-in environment variable called NODE_ENV. When run npm start, it is always equal to 'development', when run npm test it is always equal to 'test', and when run npm run build to make a production bundle, it is always equal to 'production' */
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
+    app.use(express.static(path.resolve(__dirname, '../client/build')))
     app.get(/.*/, (req, res) => {
         // res.sendFile() transfers the file at the given path and it sets the Content-Type response HTTP header field based on the filename extension.
         res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
